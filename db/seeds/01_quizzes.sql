9285dc9215cf965b0d47333a9c981d6c3babad24
Yahya Islamovic <yahya.islamovic.work@live.com>
2021-08-24 13:54:18 -0400

    fix db seeds and schema

--- db/seeds/01_quizzes.sql
+++ db/seeds/01_quizzes.sql
@@ -1,9 +1,9 @@
-INSERT INTO quizzes (user_id, name, subject, available_to_public) VALUES(1, 'Maths', 'How much do you know about the calculations? Take this quiz to find out!', FALSE);
-INSERT INTO quizzes (user_id, name, subject, available_to_public) VALUES(2, 'Science', 'Information about #life #science. Take this quiz to check your IQ!', FALSE);
-INSERT INTO quizzes (user_id, name, subject, available_to_public) VALUES(2, 'Canadian History', 'How much do you know about the canadian history? Take this quiz to find out!', FALSE);
-INSERT INTO quizzes (user_id, name, subject, available_to_public) VALUES(1, 'Geography', 'How much do you know about the different countries? Take this quiz to find out!', FALSE);
-INSERT INTO quizzes (user_id, name, subject, available_to_public) VALUES(1, 'Achronym', 'Abbreviation language #languages #english #ESL #achronym. Take this quiz to check your IQ!', FALSE);
-INSERT INTO quizzes (user_id, name, subject, available_to_public) VALUES(2, 'English Idioms', 'Figurative language work #idioms #english #ESL. Take this quiz to check your IQ!', FALSE);
-INSERT INTO quizzes (user_id, name, subject, available_to_public) VALUES(2, 'Sports', 'Are you a #sports lover? Do you enjoy practising or watching any #sport? Lets see who knows the most! Ready, steady...go!', FALSE);
-INSERT INTO quizzes (user_id, name, subject, available_to_public) VALUES(1, 'Games', 'How much do you know about the #video games? Take this quiz to find out!', FALSE);
-INSERT INTO quizzes (user_id, name, subject, available_to_public) VALUES(1, 'Music', 'An introduction to #instrumental throgh #performance and #music techniques #entertainment', FALSE);
+INSERT INTO quizzes (user_id, subject, description, available_to_public) VALUES(NULL, 'Maths', 'How much do you know about the calculations? Take this quiz to find out!', TRUE);
+INSERT INTO quizzes (user_id, subject, description, available_to_public) VALUES(NULL, 'Science', 'Information about #life #science. Take this quiz to check your IQ!', TRUE);
+INSERT INTO quizzes (user_id, subject, description, available_to_public) VALUES(NULL, 'Canadian History', 'How much do you know about the canadian history? Take this quiz to find out!', TRUE);
+INSERT INTO quizzes (user_id, subject, description, available_to_public) VALUES(NULL, 'Geography', 'How much do you know about the different countries? Take this quiz to find out!', TRUE);
+INSERT INTO quizzes (user_id, subject, description, available_to_public) VALUES(NULL, 'Achronym', 'Abbreviation language #languages #english #ESL #achronym. Take this quiz to check your IQ!', TRUE);
+INSERT INTO quizzes (user_id, subject, description, available_to_public) VALUES(NULL, 'English Idioms', 'Figurative language work #idioms #english #ESL. Take this quiz to check your IQ!', TRUE);
+INSERT INTO quizzes (user_id, subject, description, available_to_public) VALUES(NULL, 'Sports', 'Are you a #sports lover? Do you enjoy practising or watching any #sport? Lets see who knows the most! Ready, steady...go!', TRUE);
+INSERT INTO quizzes (user_id, subject, description, available_to_public) VALUES(NULL, 'Games', 'How much do you know about the #video games? Take this quiz to find out!', TRUE);
+INSERT INTO quizzes (user_id, subject, description, available_to_public) VALUES(NULL, 'Music', 'An introduction to #instrumental throgh #performance and #music techniques #entertainment', TRUE);
