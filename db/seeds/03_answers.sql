2543611bb8db25f0b59fc87433da502cbdde0be8
Pooja Patel <poojapatel.web@gmail.com>
2021-08-23 22:39:58 -0400

    Insert four different answers for each question and one of the answer is correct

---
+++ db/seeds/03_answers.sql
@@ -0,0 +1,269 @@
+INSERT INTO answers (question_id, answer, is_correct) VALUES (1, 'l*b', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (1, 'b*b', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (1, 's*s', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (1, 'h*b', FALSE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (2, '22', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (2, '31', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (2, '42', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (2, '82', FALSE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (3, 'l*b*h', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (3, '1/2b*h', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (3, 's*s', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (3, 'l*b+1/2', FALSE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (4, '2circular ends + 1 tube', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (4, '5ends', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (4, '1tube + 1tube', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (4, '2ends', FALSE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (5, '24', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (5, '32', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (5, '45', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (5, '23', FALSE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (6, '36', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (6, '48', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (6, '40', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (6, '42', FALSE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (7, 'blackfish', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (7, 'clownfish', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (7, 'whitefish', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (7, 'goldenfish', FALSE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (8, 'stem', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (8, 'roots', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (8, 'petals', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (8, 'leaves', FALSE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (9, 'from grass to insect', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (9, 'from snake to insect', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (9, 'from sun to grass', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (9, 'from grass to frog', FALSE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (10, 'leaves', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (10, 'roots', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (10, 'flower', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (10, 'stem', FALSE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (11, 'hibernation', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (11, 'reproduction', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (11, 'germination', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (11, 'production', FALSE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (12, 'skin', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (12, 'heart', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (12, 'spine', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (12, 'brain', FALSE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (13, 'Jacques Cartier', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (13, 'Queen Elizabeth I', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (13, 'Sir Laurier', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (13, 'Mr John', FALSE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (14, 'Mr John', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (14, 'Queen Elizabeth II', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (14, 'Sir Wilfrid Laurier', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (14, 'Jacques Cartier', FALSE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (15, 'Jacques Cartier', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (15, 'John A. Macdonald', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (15, 'Sir Wilfrid Laurier', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (15, 'Mr Cartier', FALSE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (16, 'Mr Cartier', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (16, 'Jacques Cartier', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (16, 'John A. Macdonald', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (16, 'Queen Elizabeth II', FALSE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (17, 'Mr Cartier', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (17, 'Jacques Cartier', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (17, 'Sir John A. Macdonald', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (17, 'Queen Elizabeth II', FALSE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (18, 'July 10, 1857', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (18, 'July 15, 1887', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (18, 'July 6, 1867', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (18, 'July 10, 1877', FALSE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (19, 'Ankara', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (19, 'Austin', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (19, 'Sacramento', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (19, 'New York', FALSE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (20, 'Manitoba', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (20, 'Alberta', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (20, 'Toronto', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (20, 'Ottawa', TRUE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (21, 'New York', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (21, 'Alberta', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (21, 'Ottawa', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (21, 'Austin', TRUE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (22, 'Arizona', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (22, 'New Delhi', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (22, 'Paris', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (22, 'China', FALSE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (23, 'Turkey', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (23, 'Washington, D.C', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (23, 'Sacramento', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (23, 'China', FALSE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (24, 'Ankara', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (24, 'Sacramento', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (24, 'New Delhi', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (24, 'Arizona', FALSE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (25, 'Lazy', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (25, 'Loud', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (25, 'Loudly', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (25, 'Lots', FALSE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (26, 'Orange', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (26, 'Ohh', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (26, 'Opinion', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (26, 'Onion', FALSE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (27, 'Person', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (27, 'People', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (27, 'Peers', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (27, 'Printer', FALSE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (28, 'Anymore', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (28, 'Any', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (28, 'Anyone', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (28, 'Anything', TRUE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (29, 'Red', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (29, 'Right', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (29, 'Righty', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (29, 'Rose', FALSE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (30, 'Great', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (30, 'God', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (30, 'Goddess', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (30, 'Good', FALSE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (31, 'Beinhg a basketball player', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (31, 'Go to Sleep', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (31, 'Relax', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (31, 'Playing in the hay', FALSE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (32, 'helping', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (32, 'Listening', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (32, 'watching', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (32, 'singing', FALSE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (33, 'Something that would never happen', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (33, 'When you leave', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (33, 'Later', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (33, 'Its raining', FALSE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (34, 'had a baby', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (34, 'Got married', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (34, 'started working', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (34, 'living together', FALSE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (35, 'eating potatoes', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (35, 'Being lazy', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (35, 'exercising', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (35, 'eating dinner', FALSE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (36, 'something fun', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (36, 'Easy', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (36, 'a Present', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (36, 'Difficult', FALSE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (37, 'arbitrator', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (37, 'a referee', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (37, 'Judge', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (37, 'controller', FALSE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (38, 'drawer', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (38, 'draw', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (38, 'pair', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (38, 'equals', FALSE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (39, 'a golf iron', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (39, 'a golf club', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (39, 'a golf man', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (39, 'a golf stick', FALSE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (40, 'a trainee', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (40, 'a guide', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (40, 'a coach', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (40, 'a couch', FALSE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (41, 'boss', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (41, 'Commander', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (41, 'Capttain', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (41, 'cheersleader', FALSE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (42, 'Ski poles', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (42, 'Ski sticks', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (42, 'Ski bats', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (42, 'Ski posts', FALSE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (43, 'Super mario bros', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (43, 'No mans land', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (43, 'Donkey Kong math', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (43, 'Donkey Kong', TRUE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (44, 'Hop man', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (44, 'Skip man', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (44, 'Jump Man', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (44, 'Mario tomorrow', FALSE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (45, '1984', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (45, '1986', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (45, '1945', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (45, '1983', TRUE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (46, 'Yuji', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (46, 'Lebadand B. Vanerd', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (46, 'Mr Arcade', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (46, 'Mr Naka', FALSE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (47, '1290', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (47, '1298', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (47, '1899', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (47, '1983', TRUE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (48, 'Quarters', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (48, 'electricity', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (48, 'Fun', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (48, 'coin', FALSE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (49, 'ABCDE', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (49, 'EGBDF', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (49, 'QWERT', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (49, 'MNJKL', FALSE);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (50, 'Flute', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (50, 'Bassoon', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (50, 'Clarinet', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (50, 'Alto Saxophone', False);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (51, 'QWER', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (51, 'ACEG', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (51, 'LKJO', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (51, 'AVGH', False);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (52, 'Cello', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (52, 'String Bass', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (52, 'Violin', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (52, 'Viola', False);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (53, 'Sight reading', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (53, 'Time signature', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (53, 'Harmony', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (53, 'Adagio', False);
+
+INSERT INTO answers (question_id, answer, is_correct) VALUES (54, 'B minor', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (54, 'G major', TRUE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (54, 'A minor', FALSE);
+INSERT INTO answers (question_id, answer, is_correct) VALUES (54, 'C major', False);
